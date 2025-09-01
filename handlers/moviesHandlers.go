206e6960c827e6f3c4986a23930ea20a8f0061e5
Magzhan <turalievmagzhan.00@mail.ru>
2025-09-01 23:18:49 +0500

    Genres Finish

--- handlers/moviesHandlers.go
+++ handlers/moviesHandlers.go
@@ -10,6 +10,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Movie struct {
+	ID          int      `json:"id" gorm:"primary_key"`
+	Title       string   `json:"title"`
+	Description string   `json:"description"`
+	Year        int      `json:"year"`
+	Director    string   `json:"director"`
+	PosterURL   string   `json:"poster_url"`
+	Genres      []string `json:"genres" gorm:"many2many:movie_genres;"`
+	TrailerURL  string   `json:"trailer_url"`
+}
+
 func PingHandler(c *gin.Context) { // Функция для обработки запроса на /ping
 	c.JSON(http.StatusOK, gin.H{
 		"message": "pong",
