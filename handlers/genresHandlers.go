33ab8dbbc104e41981c0a7848ab0ced101fab134
Magzhan <turalievmagzhan.00@mail.ru>
2025-09-06 22:39:08 +0500

    далее реализовать жанр

--- handlers/genresHandlers.go
+++ handlers/genresHandlers.go
@@ -12,4 +12,23 @@ func NewGenresHandler() *GenresHandler {
 	}
 }
 
-//реализовать методы для работы с жанрами
+// реализовать методы для работы с жанрами
+func (h *GenresHandler) FindAll(c) {
+	// получить все жанры
+}
+
+func (h *GenresHandler) FindByID(c) {
+	// получение жанра по id
+}
+
+func (h *GenresHandler) Create(c) {
+	// создание жанра
+}
+
+func (h *GenresHandler) Update(c) {
+	// обновление жанра
+}
+
+func (h *GenresHandler) Delete(c) {
+	// удаление жанра
+}
